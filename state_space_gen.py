79a046608bb497d8ce24aa00f804a0cdd90960b2
JVViacrusis <justin.viacrusis21p@gmail.com>
2024-03-18 20:04:54 -0700

    add brute force method of obtaining valid marble groups (groups of marbles that are 'in a line')

--- state_space_gen.py
+++ state_space_gen.py
@@ -29,6 +29,16 @@ informal definitions:
 """
 from pprint import pprint
 import utils
+import itertools
+
+directions = {
+    "NE" : 11,
+    "E" : 1,
+    "SE" : -10,
+    "SW" : -11,
+    "W" : -1,
+    "NW" : 10
+}
 
 
 def generate_moves_and_resulting_boardstates(in_path: str,
@@ -40,6 +50,10 @@ def generate_moves_and_resulting_boardstates(in_path: str,
     pprint(board)
     print(f"player_turn: {player_turn}")
     utils.print_board(board)
+    print("board:", board)
+
+    valid_marble_groups = gen_valid_marble_groups(board, player_turn)
+    print("valid_marble_groups:", valid_marble_groups)
 
 
 def in_to_dict(in_path: str) -> tuple[dict[int, int], int]:
@@ -62,14 +76,77 @@ def in_to_dict(in_path: str) -> tuple[dict[int, int], int]:
     return board, player_turn
 
 
-def gen_valid_marble_groups(board: dict[str, int], color: int):
+def gen_valid_marble_groups(board: dict[int, int], color: int):
     """Generates valid marble groups for a given board state."""
-    pass
+    player_marbles = board.copy()
+    _filter_for_color(player_marbles, color)
+    # print("player marbles:", player_marbles)
+    coords = list(player_marbles.keys())
+    # print("coords:", coords)
+    all_groups = _gen_combinations(coords)
+    print("all_groups:", all_groups)
+
+    _filter_for_valid_groups(all_groups)
+    valid_groups = all_groups
+    print("valid_groups:", valid_groups)
+    return valid_groups
 
 
-def filter_for_color(board: dict[int, int], color: int):
+def _filter_for_valid_groups(groups: list[int]):
+    """Filters out invalid groups."""
+    groups_copy = groups.copy()
+    to_remove = []
+    for i, group in enumerate(groups_copy):
+        if len(group) == 1:
+            continue
+
+        if len(group) == 2:
+            valid_two_length = False
+            # if not adjacent, invalid
+            for dir_val in directions.values():
+                if group[1] == group[0] + dir_val:
+                    valid_two_length = True
+            if not valid_two_length:
+                to_remove.append(i)
+            continue
+
+        if len(group) == 3:
+            valid_three_length = False
+
+            for dir_val in directions.values():
+                if (group[1] == group[0] + dir_val
+                        and group[2] == group[0] + dir_val + dir_val):
+                    valid_three_length = True
+
+            if not valid_three_length:
+                to_remove.append(i)
+            continue
+
+    for i, combo_to_remove in enumerate(to_remove):
+        groups.pop(combo_to_remove - i)
+
+
+def _filter_for_color(board: dict[int, int], color: int):
     """In place, filters the board out for the colors we are looking for."""
-    pass
+    to_remove = []
+    for key, value in board.items():
+        if value != color:
+            to_remove.append(key)
+
+    for key in to_remove:
+        del board[key]
+
+
+def _gen_combinations(marble_coords: list[int]):
+    """Generates all combinations of length 1, 2, 3 given a set of coords.
+
+    :return: The list of all combinations of length 1, 2, and 3
+    :rtype: list[tuple[]]
+    """
+    len_1 = list(itertools.combinations(marble_coords, 1))
+    len_2 = list(itertools.combinations(marble_coords, 2))
+    len_3 = list(itertools.combinations(marble_coords, 3))
+    return len_1 + len_2 + len_3
 
 
 def gen_valid_broadsides():
@@ -94,10 +171,10 @@ def gen_inlines():
 if __name__ == "__main__":
     in_base = "data/in/"
     out_base = "data/out/"
-    generate_moves_and_resulting_boardstates(in_base+"Test2.input",
+    generate_moves_and_resulting_boardstates(in_base+"Test3.input",
                                              out_base+"test1.out.moves",
                                              out_base+"test1.out.board")
 
-    generate_moves_and_resulting_boardstates(in_base+"Test1.input",
-                                             out_base+"test1.out.moves",
-                                             out_base+"test1.out.board")
+    # generate_moves_and_resulting_boardstates(in_base+"Test1.input",
+    #                                          out_base+"test1.out.moves",
+    #                                          out_base+"test1.out.board")
